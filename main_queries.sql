034a451ed8a56898bef28d6385b7c0ab3a99c231
LudovicaTo <119680854+ludovicato@users.noreply.github.com>
2023-02-15 15:44:43 +0100

    Update main_queries.sql

--- main_queries.sql
+++ main_queries.sql
@@ -22,15 +22,15 @@ LIMIT 10;
 
 
 --Top 10 blacksmiths with the largest expences in one month
-SELECT DISTINCT(EXTRACT(MONTH FROM invoice_date)) AS month, SUM(invoice_amount), blacksmith_id, region
+SELECT DISTINCT(EXTRACT(MONTH FROM invoice_date)) AS month, SUM(invoice_amount) AS monthly_amount, blacksmith_id, region
 FROM blacksmiths_in_westeros
 GROUP BY blacksmith_id, region, month
-ORDER BY sum DESC
+ORDER BY monthly_amount DESC
 LIMIT 10;
 
 
 --Check monthy invoice amount by region
-SELECT DISTINCT(EXTRACT(MONTH FROM invoice_date)) AS month, ROUND(SUM(invoice_amount), 2) AS sum, region
+SELECT DISTINCT(EXTRACT(MONTH FROM invoice_date)) AS month, ROUND(SUM(invoice_amount), 2) AS monthly_amount, region
 FROM blacksmiths_in_westeros
 GROUP BY month, region
 ORDER BY region;
@@ -42,11 +42,24 @@ FROM blacksmiths_in_westeros
 GROUP BY months;
 
 
+--Check total number of invoices by region
+SELECT region, COUNT(invoice_id) as number_invoices
+FROM blacksmiths_in_westeros
+GROUP BY region;
+
+
 --Check total number of invoices by city
 SELECT city, count(*)
 FROM blacksmiths_in_westeros
 GROUP BY city
 ORDER BY count DESC;
+
+
+--Check total invoice amount by region
+SELECT region, SUM(invoice_amount) as total_amount
+FROM blacksmiths_in_westeros
+GROUP BY region
+ORDER BY total_amount DESC;
 
 
 --Check total number of blacksmiths in each region
@@ -57,10 +70,10 @@ ORDER BY num DESC;
 
 
 --Check total number of blacksmiths in each province
-SELECT COUNT(DISTINCT blacksmith_id) AS farm_id, region, province
+SELECT COUNT(DISTINCT blacksmith_id) AS blacksmith_id, region, province
 FROM blacksmiths_in_westeros
 GROUP BY region, province
-ORDER BY farm_id DESC;
+ORDER BY blacksmith_id DESC;
 
 --Check if number of ID and number of names matches
 SELECT COUNT(DISTINCT(blacksmith_id)) AS num_blacksmith_ids, COUNT(DISTINCT(blacksmith_name)) AS num_blacksmith_names
@@ -92,7 +105,7 @@ ORDER BY name;
 
 
 --Check number of suppliers
-SELECT COUNT(DISTINCT(supplier)) AS fornitori
+SELECT COUNT(DISTINCT(supplier)) AS suppliers
 FROM blacksmiths_in_westeros;
 
 
@@ -107,43 +120,61 @@ LIMIT 10;
 
 
 --Number of blacksmiths provided by each supplier
-SELECT supplier, COUNT(DISTINCT blacksmith_id) as num_farmacie
+SELECT supplier, COUNT(DISTINCT blacksmith_id) as number_blacksmiths
 FROM blacksmiths_in_westeros
 GROUP BY supplier
-ORDER BY num_farmacie DESC;
+ORDER BY number_blacksmiths DESC;
 
 
 --Number of suppliers each blacksmiths has
-SELECT blacksmith_id, COUNT(DISTINCT supplier) as num_fornitori
+SELECT blacksmith_id, COUNT(DISTINCT supplier) as number_suppliers
 FROM blacksmiths_in_westeros
 GROUP BY blacksmith_id
-ORDER BY num_fornitori DESC;
+ORDER BY number_suppliers DESC;
 
 
 --Number of regions each supplier works in
-SELECT supplier, COUNT(DISTINCT region) as regioni
+SELECT supplier, COUNT(DISTINCT region) as regions
 FROM blacksmiths_in_westeros
 GROUP BY supplier
-ORDER BY regioni DESC;
+ORDER BY regions DESC;
 
 
 --Distribution of suppliers across different regions of operation
-SELECT DISTINCT(regioni) AS num_regioni_fornite, COUNT(supplier) AS forn
+SELECT DISTINCT(regioni) AS number_regions, COUNT(supplier) AS number_suppliers
 FROM
-	(SELECT supplier, COUNT(DISTINCT region) as regioni
+	(SELECT supplier, COUNT(DISTINCT region) as regions
 	FROM blacksmiths_in_westeros
 	GROUP BY supplier) AS reg
-GROUP BY num_regioni_fornite
-ORDER BY num_regioni_fornite DESC;
+GROUP BY number_regions
+ORDER BY number_regions DESC;
 
 
 --Top 10 suppliers by number of invoices with details on total amount
-SELECT supplier, ROUND(SUM(invoice_amount),2) as totale_importi, COUNT(invoice_id) AS numero_fatture, COUNT(DISTINCT region) AS regioni
+SELECT supplier, ROUND(SUM(invoice_amount),2) as total, COUNT(invoice_id) AS number_invoices, COUNT(DISTINCT region) AS regions
 FROM blacksmiths_in_westeros
 GROUP BY supplier
-ORDER BY numero_fatture DESC
+ORDER BY number_invoices DESC
 LIMIT 10;
 
 
+--Distribution of total imports through time
+SELECT invoice_date, ROUND(AVG(invoice_amount),2) as avg_invoice_amount
+FROM blacksmiths_in_westeros
+GROUP BY invoice_date
+ORDER BY invoice_date ASC;
 
+
+--Distribution of the number of invoices through time
+SELECT date_trunc('month', invoice_date) as month, COUNT(invoice_id) as number_invoices
+FROM blacksmiths_in_westeros
+GROUP BY month
+ORDER BY month;
+
+
+--Average invoice amount by supplier through time
+SELECT supplier, invoice_date, COUNT(invoice_id) as number_invoices, ROUND(AVG(invoice_amount),2) as avg_invoice_amount
+FROM blacksmiths_in_westeros
+GROUP BY supplier, invoice_date
+ORDER BY supplier, invoice_date ASC;
 
